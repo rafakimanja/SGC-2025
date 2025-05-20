f312ab1ddcdb802e293f5dd9dbb390a4b843b7de
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2025-05-19 22:48:34 -0300

    alter contente Class3

--- Lab 4/Classe3.java
+++ Lab 4/Classe3.java
@@ -1,5 +1,5 @@
  public class classe3 {
        public static void main(String[] args) {
-           System.out.println("Versão base");
+           System.out.println("Modificado no new_featureB");
        }
    }
